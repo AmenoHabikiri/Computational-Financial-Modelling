f9a3a2f527df8c6894e950e12662506c52ba00d6
AmenoHabikiri <sagartiitm23@gmail.com>
2022-05-26 15:10:37 +0530

    finding yearly  volatility of stocks

--- Lab2/Q1.py
+++ Lab2/Q1.py
@@ -1,3 +1,15 @@
 import pandas as pd
+import math
+from matplotlib import pyplot as plt
+from numpy import log
+import numpy as np
 data = pd.read_csv('StocksData.csv')
+data[['TCS','ASIANPAINT','BRITANNIA']].plot(label='ClosePreice over the years', figsize=(16, 8))
+#plt.show()
+data[['LOGTCS','LOGASIANPAINT','LOGBRITANNIA']] = data[['TCS','ASIANPAINT','BRITANNIA']].pct_change().apply(lambda x: np.log(1+x))
+SD=[0,0,0]
 print(data)
+SD[0]=np.sqrt((data['LOGTCS'].var())*250)
+SD[1]=np.sqrt((data['LOGASIANPAINT'].var())*250)
+SD[2]=np.sqrt((data['LOGBRITANNIA'].var())*250)
+print(SD)
